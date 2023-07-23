93c75d6cace0e673f373398a02a7d754dbac983d
Wishtrader <mr.akomarov@gmail.com>
2023-07-23 22:56:14 +0300

    landing page template created

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,51 @@
 import './App.css'
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import {
+  AddJob,
+  HomeLayout,
+  EditJob,
+  Stats,
+  AllJobs,
+  Login,
+  Landing,
+  Profile,
+  DashboardLayout,
+  Error,
+  Register,
+  Admin
+} from "./pages";
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />
+      },
+      {
+        path: 'register',
+        element: <Register />
+      },
+      {
+        path: 'login',
+        element: <Login />
+      },
+      {
+        path: 'dashboard',
+        element: <DashboardLayout />
+      },
+    ]
+  },
+])
+
 
 function App() {
 
   return (
-    <>
-      <h1 className="">Jobify</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
---
+++ client/src/pages/DeleteJob.jsx
@@ -0,0 +1,9 @@
+const DeleteJob = () => {
+  return (
+    <div>
+      <h1>Delete Job Page</h1>
+    </div>
+  );
+};
+
+export default DeleteJob;
