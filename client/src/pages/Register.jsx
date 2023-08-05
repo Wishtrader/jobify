6d477045cf9f9c5d58ac691df368483e9c6955cd
Wishtrader <mr.akomarov@gmail.com>
2023-08-06 02:32:13 +0300

    login and register pages created

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,11 +1,25 @@
 import {Link} from "react-router-dom";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import {FormRow, Logo} from "../components";
 
 const Register = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
-      <Link to='/login' >Login Page</Link>
-    </div>
+    <Wrapper>
+      <form className='form'>
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type='text' name='name' defaultValue='John' labelText='first name' />
+        <FormRow type='text' name='lastName' defaultValue='Smith' labelText='last name' />
+        <FormRow type='text' name='location' defaultValue='Earth' labelText='location' />
+        <FormRow type='email' name='email' defaultValue='john@gmail.com' labelText='email' />
+        <FormRow type='password' name='password' defaultValue='john123' labelText='password' />
+        <button type='submit' className='btn btn-block' >submit</button>
+        <p>
+          Already member?
+          <Link to='/login' className='member-btn' >Login</Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 
